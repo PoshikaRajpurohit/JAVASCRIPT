4562ee3798a109411bebba46135a4ddde1c5ce91
PoshikaRajpurohit <159758700+PoshikaRajpurohit@users.noreply.github.com>
2024-12-05 11:09:11 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,97 @@
+// OPERATOR-PROGRAMS (5-12-24)
+
+
+// #1 Convert dollar into rupees.
+
+    // let rate=83 ,dollar=10;
+    // rupee=dollar*rate;
+    // console.log("Total rupee=",rupee);
+
+
+// #2 Convert feet into inches.
+
+    // let inches=12,feet=5;
+    // convert=feet*inches;
+    // console.log("Feets=",feet);
+    // console.log(" Conversion of feet into inches=",convert);
+
+
+//  #3 Convert meter into inches.
+
+    // let inches=39.3701, meter=5;  
+    // conversion=meter*inches;
+    // console.log("Meters=",meter);
+    // console.log("Conversion of meter into inches=",conversion);  
+
+
+// #4 Convert cels into farehnit.
+
+// let temp=200;
+// f=(1.8*temp)+32;
+// console.log("The temperature in celsius=",temp);
+// console.log("The temperature in Fahrenheit=",f);
+
+
+// #5 Findout area of circle.
+
+// let radius=10;
+// area=3.14*radius**2;
+// console.log("The radius of the circle=",radius);
+// console.log("The area of the circle=",area);
+
+
+// #6 Findout Area of Triangle
+
+// let b=5 ,h=7;
+// area=0.5*b*h;
+// console.log("The Breadth  of Triangle is=",b);
+// console.log("The Height  of Triangle is=",h);
+// console.log("The Area of Triangle=",area);
+
+
+// #7 Findout Area of rectangle
+
+// let length=20 ,width=15;
+// area=length*width;
+// console.log("The length of rectangle=",length);
+// console.log("The width  of rectangle=",width);
+// console.log("The Area of Rectangle=",area);
+
+
+// #8 Findout Simple Interest
+
+// let p=2000,r=5,t=5;
+// simple=p*r*t/100
+// console.log("The principal amount=",p);
+// console.log("The rate of interest=",r);
+// console.log("The time period=",t);
+// console.log("The simple interest=",simple);
+
+
+// #9 Findout three sub total and percentage
+
+// let eng=68 ,sci=89 ,ss=95;
+// obtainedmarks=eng+sci+ss;
+// percentage=obtainedmarks*100/300;
+// console.log("Marks Obtained in English=",eng);
+// console.log("Marks Obtained in Science=",sci);
+// console.log("Marks Obtained in S.S=",ss);
+// console.log("Percentage=",percentage);
+
+
+// #10 Employee salary 
+
+// let basicSlr=20000 ,hra ,da;
+// hra=basicSlr*0.15;
+// da=basicSlr*0.08;
+// totalSalary=basicSlr+hra+da;
+// console.log("The Basic salary of an employee=",basicSlr);
+// console.log("The HRA of an employee=",hra);
+// console.log("The DA  of an employee=",da);
+// console.log("The total salary of an employee=",totalSalary);
+
+
+
+
+
+
